0e04ec62c2bf77c0728cbe29c1accefee6f798d7
stefano <stefano.gonella98@gmail.com>
2020-04-17 19:09:25 +0200

    #Splitted in function code and other optimization

--- main2.py
+++ main2.py
@@ -2,6 +2,7 @@ import face_recognition
 import cv2
 import os
 import subprocess
+import numpy
 
 
 def spostaFile(image_base, video, y):
@@ -39,13 +40,37 @@ def spostaFile(image_base, video, y):
 
 ####################################################################
 
-def caricaDaFile(num_lines_num_parsed,lista_video,file):
-    for v in range(num_lines_video_num_parsed):  ## carico all'interno dell'array
+def caricaDaFile(num_val,file):
+    list=[]
+    for v in range(int(num_val)):  ## carico all'interno dell'array
 
-        lista_video.append(file.readline().rstrip())
+        list.append(file.readline().rstrip())
 
     file.close()
 
+    return list
+
+
+def imageEncodeList():
+    image = []
+    for i in os.listdir(LISTA_IMMAGINI_PATH):
+        if not i.endswith(".txt"):
+            try:
+
+                image_frame = face_recognition.load_image_file(LISTA_IMMAGINI_PATH + i)
+                cod = face_recognition.face_encodings(image_frame)[0]
+                image.append(cod)
+            except:
+                print(f"nessun immagine {LISTA_IMMAGINI_PATH + i}")
+
+    return image
+
+def getUniqueValueList(image_list):
+    myset = set(image_list)
+    unique=[]
+    unique=[item for item in myset if item not in unique]
+    print(unique)
+    return unique
 
 
 print("inizio")
@@ -58,9 +83,7 @@ if int(out)==0 :
     subprocess.check_output("echo '/run/media/stegon/131ac7e7-32e9-457b-92e0-7068ecf6c2d7/MEGA2/' >>config.cfg ", shell=True).rstrip()
     subprocess.check_output("echo '/home/stegon/PycharmProjects/LOADER/' >>config.cfg ", shell=True).rstrip()
 
-
 fileCfg = open('config.cfg', 'r')
-
 
 LISTA_IMMAGINI_PATH=fileCfg.readline().rstrip()
 LISTA_VIDEO_PATH=fileCfg.readline().rstrip()
@@ -72,19 +95,19 @@ fileCfg.close()
 
 
 
-subprocess.call("find " + LISTA_IMMAGINI_PATH + " -maxdepth 1 -type f -exec basename \"{}\" > " +LISTA_IMMAGINI_PATH+"lista.txt \\;", shell=True)# sum(1 for line in open('VALORI.txt'))
-subprocess.call("find " + LISTA_VIDEO_PATH + " -maxdepth 1 -type f -exec basename \"{}\" > " +LISTA_VIDEO_PATH+"lista.txt \\;", shell=True)# sum(1 for line in open('VALORI.txt'))
+subprocess.call("find " + LISTA_IMMAGINI_PATH + " -maxdepth 1 -type f -exec basename \"{}\" > " "lista_image.txt \\;", shell=True)# sum(1 for line in open('VALORI.txt'))
+subprocess.call("find " + LISTA_VIDEO_PATH + " -maxdepth 1 -type f -exec basename \"{}\" > " "lista_video.txt \\;", shell=True)# sum(1 for line in open('VALORI.txt'))
 
 
 
-num_lines_valori_num=subprocess.check_output(['wc', '-l', LISTA_IMMAGINI_PATH+"lista.txt"]).decode("utf-8")
-num_lines_video_num=subprocess.check_output(['wc', '-l', LISTA_VIDEO_PATH+"lista.txt"]).decode("utf-8")
+num_lines_valori_num=subprocess.check_output(['wc', '-l', "lista_image.txt"]).decode("utf-8")
+num_lines_video_num=subprocess.check_output(['wc', '-l', "lista_video.txt"]).decode("utf-8")
 
 print(f"sono presenti %s valori nel file valori " % (num_lines_valori_num[:3]))
 print(f"sono presenti %s valori nel file lista video  " % (num_lines_video_num[:3]))
 
-fileVal = open(LISTA_IMMAGINI_PATH+'lista.txt', 'r')
-fileVal_lista = open(LISTA_VIDEO_PATH+'lista.txt', 'r')
+fileVal = open('lista_image.txt', 'r')
+fileVal_lista = open('lista_video.txt', 'r')
 # lista_video=[]
 # lista_video.append(fileVal_lista.readline())
 image_base = []
@@ -94,12 +117,18 @@ check = False
 num_lines_video_num_parsed=int(num_lines_video_num[:2])
 num_lines_valori_num_parsed=int(num_lines_valori_num[:2])
 
-caricaDaFile(num_lines_video_num_parsed,lista_video,fileVal_lista)
-caricaDaFile(num_lines_valori_num,image_base,fileVal)
+lista_video=caricaDaFile(num_lines_video_num_parsed,fileVal_lista)
+image_base=caricaDaFile(num_lines_valori_num_parsed,fileVal)
 
+image_aus=[]
+for i in range(num_lines_valori_num_parsed):
+    image_aus.append(image_base[i].lower()[:-4])
 
+image_unique_list=getUniqueValueList(image_aus)
+print(image_unique_list)
 
-# print(lista_video[v])
+image_encoded=imageEncodeList()
+
 
 
 
@@ -110,99 +139,88 @@ for k in range(num_lines_video_num_parsed):
     video = LISTA_VIDEO_PATH+lista_video[k].rstrip()
     print(video)
     vidcap = cv2.VideoCapture(video)
-    #success, image = vidcap.read()
-    success=True
+    success, image = vidcap.read()
+    #success=True
     count = 1000
     count_matches=[0]*num_lines_valori_num_parsed
+    count_matches_unique=[0]*len(image_unique_list)
 
     #while count < 150001 and check == False and video!=LISTA_VIDEO_PATH+"lista.txt":
-    while success ==True and check == False and video!=LISTA_VIDEO_PATH+"lista.txt":
+    while success ==True and check == False :
         ##TO DO impostare un triplo check per essere sicuri del match
         ##success, image = vidcap.read()
-        vidcap.set(cv2.CAP_PROP_POS_FRAMES, count)
+        ##vidcap.set(cv2.CAP_PROP_POS_FRAMES, count)
         success, image = vidcap.read()
-        print("aa")
 
         #success, image = vidcap.read()
 
-       ## if (count % 1000 == 0 and count > 1):
+        if (count % 250 == 0 and count > 1):
 
-        print(LOADER_FRAME_DIR+"frame%d.jpg" % count)
-        filename = LOADER_FRAME_DIR+"frame" + str(count) + ".jpg"
-        print(filename)
-        cv2.imwrite(filename, image)  # save frame as JPEG file
+            #print(LOADER_FRAME_DIR+"frame%d.jpg" % count)
+            filename = LOADER_FRAME_DIR+"frame" + str(count) + ".jpg"
+            print(filename)
+            cv2.imwrite(filename, image)  # save frame as JPEG file
 
-        # confronto immagini
-        image_frame = face_recognition.load_image_file(filename)
-        doCheck=0
+            # confronto immagini
+            image_frame = face_recognition.load_image_file(filename)
+            doCheck=0
 
-        try:
+            try:
 
-            image_frame_encode = face_recognition.face_encodings(image_frame)[0]
+                image_frame_encode = face_recognition.face_encodings(image_frame)[0]
 
-        except:
-            print("non ci sono volti nell'immagine frame")
-            image_frame_encode = None
-            doCheck=1
+            except:
+                print("non ci sono volti nell'immagine frame")
+                image_frame_encode = None
+                doCheck=1
 
 
-        for y in range(num_lines_valori_num_parsed):  ##
+            for y in range(num_lines_valori_num_parsed):  ##
 
-            if LISTA_IMMAGINI_PATH+image_base[y]!=LISTA_IMMAGINI_PATH+"lista.txt" and doCheck==0 :
-                res = []
-                # res[0]=False
-            #    print("utilizzo l'immagine %s" %(LISTA_IMMAGINI_PATH+image_base[y]))
-                image_conf = face_recognition.load_image_file(LISTA_IMMAGINI_PATH+image_base[y])
-                image_conf_encode = face_recognition.face_encodings(image_conf)[0]
-                #print(image_conf_encode)
-                #print(image_frame_encode)
+                if  doCheck==0 :
+                    res = []
+                    # res[0]=False
+                #    print("utilizzo l'immagine %s" %(LISTA_IMMAGINI_PATH+image_base[y]))
+                   # image_conf = face_recognition.load_image_file(LISTA_IMMAGINI_PATH+image_base[y])
+                   # image_conf_encode = face_recognition.face_encodings(image_conf)[0]
 
-                try:
-                    res = face_recognition.compare_faces([image_conf_encode], image_frame_encode)
-                except:
-                    print("nessun match tra le immagini")
 
-                try:
-                    exit_attempt = res[0]
-                except:
-                    exit_attempt = False
+                    #print(image_frame_encode)
 
-                if exit_attempt == True:
-                    count_matches[y]=count_matches[y]+1
-                    print("LA PERSONA E LA STESSA STONKS aggiungo un match,attualmente sono %s" % (count_matches[y]))
-                    if count_matches[y]>=3 :
-                        spostaFile(LISTA_IMMAGINI_PATH+image_base[y], video, y)
-                        check = True
-                        break
+                    try:
+                       # res = face_recognition.compare_faces([image_conf_encode], image_frame_encode)
+                       res = face_recognition.compare_faces([image_encoded[y]], image_frame_encode)
+                       #correct = face_recognition.face_distance([image_encoded[y]], image_frame_encode)
+                       #print(correct[0])
+                       #if correct[0]<=0.6:
+                       #    res[0]=True
+
+                    except:
+                        print("nessun match tra le immagini")
+
+                    try:
+                        exit_attempt = res[0]
+                    except:
+                        exit_attempt = False
+
+                    if exit_attempt == True:
+                        count_matches[y]=count_matches[y]+1
+                        for n in range(len(image_unique_list)):
+                            if (image_base[y].lower()[:-4]) ==image_unique_list[n]:
+                                count_matches_unique[n]=count_matches_unique[n]+1
+                                print(f"LA PERSONA E LA STESSA STONKS ,attualmente sono {count_matches[y]} - trovato match con {image_unique_list[n]}")
+                            if count_matches_unique[n]>=10 :
+                                spostaFile(LISTA_IMMAGINI_PATH+image_unique_list[n], video, y)
+                                check = True
+                                for prova in range(len(image_unique_list)):
+                                    print(f"{image_unique_list[prova]} -  {count_matches_unique[prova]}")
+                                break
 
 
 
         if success==False:
             print("non ho trovato nessun match sposto in altro")
             spostaFile("ALTRO",video,y)
-        count += 1000
-
-# image_know=face_recognition.load_image_file("/home/stegon/PycharmProjects/riconoscitore/enrico1.jpg");
-# image2_know=face_recognition.load_image_file("/home/stegon/PycharmProjects/riconoscitore/andreagale.png")
-# image_unknow=face_recognition.load_image_file("/home/stegon/PycharmProjects/riconoscitore/andreagale.png")
+        count += 1
 
 
-# print(type(image_know))
-# print(f"{image_know}")
-
-# know_encoding=face_recognition.face_encodings(image_know) ##potrebbero tornare piu valori dati da piu volti ATTENZIONE
-# unknow_encoding=face_recognition.face_encodings(image2_know); ##potrebbero tornare piu valori dati da piu volti ATTENZIONE
-
-# len(unknow_encoding)
-##embedding dell'immagine
-
-# print (know_encoding)
-
-# risultato=face_recognition.compare_faces([know_encoding],unknow_encoding)
-
-# print(risultato)
-
-# if risultato[0] == True :
-#   print("LA PERSONA E LA STESSA STONKS")
-# else:
-#   print("LA PERSONA NON E LA STESSA")
